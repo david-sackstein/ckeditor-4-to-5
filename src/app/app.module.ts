01831f1451e8cc47035257bdf708486a365efeb7
David Sackstein <davids@codeprecise.com>
2023-09-16 16:07:29 +0300

    Added ckeditor-4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CKEditorModule as CKEditorModule4 } from 'ckeditor4-angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CKEditorModule4
   ],
   providers: [],
   bootstrap: [AppComponent]
